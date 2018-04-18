e4e01dc0076c9d88e003f5b6bdd06c3f6fa95134
Kelli Parris <kelli@hsdesignhouse.com>
2018-04-18 12:06:52 -0500

    first with content

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+	def home
+	end
+
+	def location
+	end
+
+	def charters
+	end
+
+	def faq
+	end
+
+	def toy_time
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+	get 'charters', to: 'static_pages#charters'
+	get 'faq', to: 'static_pages#faq'
+	get 'location', to: 'static_pages#location'
+	get 'toy-time', to: 'static_pages#toy-time'
 end
